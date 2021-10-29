cad35fecad681a9185f92f7046eb64f8cf70685a
pedroaugustomelo <pedro.aom@hotmail.com>
2021-10-29 17:06:32 -0300

    Add files via upload

---
+++ Gaussian Package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binormal_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['binormal_distributions'],
+      author= 'Pedro Augusto Oliveira Melo',
+      author_email= 'pedromelonet22@gmail.com',
+      zip_safe=False)
